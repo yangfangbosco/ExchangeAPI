16aabf673ab6ee6579a85e2ffe1e773fa4837f52
yangfangbosco <40671447+yangfangbosco@users.noreply.github.com>
2018-08-23 16:21:30 -0700

    add your database info

--- API_OB_Kraken.py
+++ API_OB_Kraken.py
@@ -12,9 +12,7 @@ logtime =  datetime.datetime.now()
 logging.info("Starting the program at " + str(logtime));
 
 try:
-    Kraken_cnx = mysql.connector.connect(user='dataapplab', password = 'gaojizhongxue123',
-                                     host='exchangedata.cluster-c4dhxbzodofv.us-west-2.rds.amazonaws.com',
-                                     database='KrakenDB')
+    Kraken_cnx = mysql.connector.connect(add your database info)
 except:
     logging.error('not able to connect to database');
 
